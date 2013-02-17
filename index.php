9b9f1b28c164629da2a82b4d15e91e0b8169b588
deeclinch <butterflie.dee@gmail.com>
2013-02-17 20:18:09 +0000

    worked on SCSS and markup

--- index.php
+++ index.php
@@ -54,17 +54,15 @@
     </nav>
     <div class="headerbg">
         <div class="headimg">
+            <div class="wrapper">
 
-            <div class="row">
-                <div class="col col6-12">
-                    <div class="col_pad">
+                <div class="row">
+                    <div class="col logo_main">
                         <a href="/">
                             <img src="/assets/img/sixpastthree-logo.png" alt="Six Past Three">
                         </a>
                     </div>
-                </div>
-                <div class="col col6-12">
-                    <div class="col_pad">
+                    <div class="col search_form fr">
                         <form action="" id="sform" method="post" class="sform">
                             <label for="s" class="label sform_label">Search Me...</label>
                             <input type="text" id="s" name="s" value="" class="sform_input input_text" placeholder="Search Me...">
@@ -73,131 +71,100 @@
                     </div>
                 </div>
             </div>
-            
+
         </div>
     </div>
-</header>
-
-<!--nav class="tnav">
-    <div class="wrapper">
-        <ul>
-            <li><a href="/members/">03:06 Members</a></li>
-            <li><a href="/staff/">03:06 Staff</a></li>
-            <li><a href="/rpg/">03:06 RPG's</a></li>
-            <li><a href="/wiki/">03:06 Wiki</a></li>
-            <li><a href="/social/">03:06 Social</a></li>
-            <li><a href="/news/">03:06 News</a></li>
-            <li><a href="/stats/">03:06 Stats</a></li>
-            <li><a href="/contact/">Contact 03:06</a></li>
-        </ul>
-    </div>
-</nav>
-<header>
-    <div class="header__img">
-        <div class="wrapper">
-            <div class="row">
-                <div class="span6 fl">
-                    <a href="/" class="logo"><img src="/assets/img/sixpastthree.png" alt="Six Pas Three. Where procrastination has no bounds" class="logo__img"></a>
-                </div>
-                <div class="span6 fr">
-                    <form action="/" method="POST" class="search">
-                        <input type="text" class="search__input" name="search" id="search" placeholder="Search Me....">
-                        <input type="submit" class="search__submit">
-                    </form>
-                </div>
-            </div>
+    <nav>
+        <div class="bnav">
+            <ul class="bnav_list">
+                <li class="bnav_item">Articles:</li>
+                <li class="bnav_item"><a href="/staff/" class="bnav_link">Games</a></li>
+                <li class="bnav_item"><a href="/rpg/" class="bnav_link">Music</a></li>
+                <li class="bnav_item"><a href="/wiki/" class="bnav_link">Film &amp; TV</a></li>
+                <li class="bnav_item"><a href="/social/" class="bnav_link">Current Events</a></li>
+                <li class="bnav_item"><a href="/news/" class="bnav_link">Art &amp; Photography</a></li>
+                <li class="bnav_item"><a href="/stats/" class="bnav_link">Lifestyle</a></li>
+                <li class="bnav_item"><a href="/contact/" class="bnav_link">Gallery</a></li>
+                <li class="bnav_item"><a href="/spm/" class=""><span class="bt_text">Join 03:06</span><span class="bt_icon"></a></li>
+            </ul>
         </div>
-    </div>
+    </nav>
 </header>
-<nav class="bnav">
-    <div class="wrapper">
-        <ul>
-            <li>Articles:</li>
-            <li><a href="/staff/">Games</a></li>
-            <li><a href="/rpg/">Music</a></li>
-            <li><a href="/wiki/">Film &amp; TV</a></li>
-            <li><a href="/social/">Current Events</a></li>
-            <li><a href="/news/">Art &amp; Photography</a></li>
-            <li><a href="/stats/">Lifestyle</a></li>
-            <li><a href="/contact/">Gallery</a></li>
-        </ul>
-        <a href="/spm/" class="fr bigassbutton"><span class="bt__text">Join 03:06</span><span class="bt__icon"></a>
-    </div>
-</nav>
 
-<div class="slider whitebg">
+<div class="whitestrip">
     <div class="wrapper">
-        <div class="rel shadow">
-            <div id="tehslider">
-                <ul>
-                    <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
-                    <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
-                    <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
-                </ul>
-            </div>
+        <div class="slider">
+            <ul>
+                <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
+                <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
+                <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
+            </ul>
         </div>
     </div>
 </div>
 
-<div class="into greybg">
+<div class="greystrip">
     <div class="wrapper">
+
         <div id="features" class="row">
-            <a href="" class="features__link span4">
+            <a href="" class="col features_link">
                 <img src="/assets/img/feature.jpg" alt="">
-                <span class="title">Feature Link Title</span>
+                <span class="features_title">Feature Link Title</span>
             </a>
-            <a href="" class="features__link span4">
+            <a href="" class="col features_link">
                 <img src="/assets/img/feature.jpg" alt="">
-                <span class="title">Feature Link Title</span>
+                <span class="features_title">Feature Link Title</span>
             </a>
-            <a href="" class="features__link span4">
+            <a href="" class="col features_link">
                 <img src="/assets/img/feature.jpg" alt="">
-                <span class="title">Feature Link Title</span>
+                <span class="features_title">Feature Link Title</span>
             </a>
-            <a href="" class="features__link span4">
+            <a href="" class="col features_link">
                 <img src="/assets/img/feature.jpg" alt="">
-                <span class="title">Feature Link Title</span>
+                <span class="features_title">Feature Link Title</span>
             </a>
         </div>
 
         <div class="row">
-            <div class="welcome span11 block">
-                <h1 class="h2 title">Hello Fellow Procrastinators and Welcome to Six Past <span class="featcolor">Three</span></h1>
-                <div class="block__content">
-                    <p>For almost a decade the Six Past Three group have been a collaboration of unique minds seeking refuge from their busy lives. People who are creative and imaginative seeking equally <a href="/forum">creative outlets</a>. 
-                        Your visit to our humble home can consist of ....</p>
-                        <ul>
-                            <li>Participating in a diverse range of topics from the serious to just plain stupid.</li>
-                            <li>Contributing *squee(s)* to a range fan forum discussions and articles.</li>
-                            <li>Getting to know people from all around the world with different interests.</li>
-                            <li>Showcasing your creative aptitudes and receiving instant feedback.</li>
-                            <li>Learning something about yourself you may not have fully realised among new friends.</li>
-                        </ul>
-                        <p>The possibilities are endless.</p>
-                        <p>So with the off chance we do actually inspire something within you or even just because we're somewhat entertaining, then by all means- come join in on the fun. You may also eventually start calling Six Past Three home too.</p>
+            <div class="col block">
+                <h1 class="h2 block_title">Hello Fellow Procrastinators and Welcome to Six Past <span class="featcolor">Three</span></h1>
+                <div class="block_content">
+                    <p>For almost a decade the Six Past Three group have been a collaboration of unique minds seeking refuge from their busy lives. People who are creative and imaginative seeking equally <a href="/forum">creative outlets</a>.</p>
+                    <p>Your visit to our humble home can consist of ....</p>
+                    <ul>
+                        <li>Participating in a diverse range of topics from the serious to just plain stupid.</li>
+                        <li>Contributing *squee(s)* to a range fan forum discussions and articles.</li>
+                        <li>Getting to know people from all around the world with different interests.</li>
+                        <li>Showcasing your creative aptitudes and receiving instant feedback.</li>
+                        <li>Learning something about yourself you may not have fully realised among new friends.</li>
+                    </ul>
+                    <p>The possibilities are endless.</p>
+                    <p>So with the off chance we do actually inspire something within you or even just because we're somewhat entertaining, then by all means- come join in on the fun. You may also eventually start calling Six Past Three home too.</p>
 
-                        <img src="/assets/img/team-signature.png" alt="teh 03:06 Team xxx" class="block__signature">
+                    <img src="/assets/img/team-signature.png" alt="teh 03:06 Team xxx" class="block_signature">
                 </div>
             </div>
-            <div class="welcome span5 block">
+            <div class="col block">
                 <h2 class="h2 title">Six Past <span class="featcolor">Three</span> News Categories</h2>
-                <div class="block__content-nav">
+                <div class="block_content-nav">
                     <nav>
-                        <ul class="block__menu">
-                            <li class="block__li"><a href="" class="block__link">Forum Modifications</a></li>
-                            <li class="block__li"><a href="" class="block__link">New Features</a></li>
-                            <li class="block__li"><a href="" class="block__link">Art Challenge Results</a></li>
-                            <li class="block__li"><a href="" class="block__link">Category Title Extras</a></li>
-                            <li class="block__li"><a href="" class="block__link">Category Title Extras</a></li>
-                            <li class="block__li"><a href="" class="block__link">Category Title Extras</a></li>
-                            <li class="block__li"><a href="" class="block__link">Category Title Extras</a></li>
+                        <ul class="block_menu">
+                            <li class="block_li"><a href="" class="block_link">Forum Modifications</a></li>
+                            <li class="block_li"><a href="" class="block_link">New Features</a></li>
+                            <li class="block_li"><a href="" class="block_link">Art Challenge Results</a></li>
+                            <li class="block_li"><a href="" class="block_link">Category Title Extras</a></li>
+                            <li class="block_li"><a href="" class="block_link">Category Title Extras</a></li>
+                            <li class="block_li"><a href="" class="block_link">Category Title Extras</a></li>
+                            <li class="block_li"><a href="" class="block_link">Category Title Extras</a></li>
                         </ul>
                     </nav>
                 </div>
             </div>
-        </div>
+        </div>        
+
     </div>
-</div-->
+</div>
+
 <?php 
   include_once('layout/footer-assets.php');
 ?>
