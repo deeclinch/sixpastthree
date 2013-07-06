d56d62e21669da4457f213381c3737ac3342eb0b
deeclinch <butterflie.dee@gmail.com>
2013-07-06 20:44:28 +0100

    update sass again and begin rewriting

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php 
-  //include_once('layout/head.php');
-?>
 <!doctype html>
 <!--[if lt IE 7]> <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie10 lt-ie9 lt-ie8" lang="en"> <![endif]-->
@@ -8,28 +5,28 @@
 <!--[if IE 9]>    <html class="no-js lt-ie10" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <title>Six Past Three website</title>  
-  <meta name="description" content=" ">
-  <meta name="keywords" content="">
-  <meta name="author" content="">
+    <title>Six Past Three website</title>  
+    <meta name="description" content=" ">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
 
-  <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width">
 
-  <meta property="og:site_name" content="Six Past Three"/>
-  <meta property="og:url" content="sixpastthree.com"/>
-  <meta property="og:title" content="Six Past Three" />
-  <meta property="og:type" content="article"/>
-  <meta property="og:description" content=" " />
-  <meta property="og:image" content=" assets/img/logo.png"/>
+    <meta property="og:site_name" content="Six Past Three"/>
+    <meta property="og:url" content="sixpastthree.com"/>
+    <meta property="og:title" content="Six Past Three" />
+    <meta property="og:type" content="article"/>
+    <meta property="og:description" content=" " />
+    <meta property="og:image" content=" assets/img/logo.png"/>
 
-  <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" />
-  <link rel="canonical" href=" " />
-  <link rel="stylesheet" href="/assets/css/site.min.css">
-  <script src="/assets/js/libs/modernizr-2.5.3.min.js"></script>
+    <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" />
+    <link rel="canonical" href=" " />
+    <link rel="stylesheet" href="/assets/css/site.min.css">
+    <script src="/assets/js/libs/modernizr-2.5.3.min.js"></script>
 </head>
 <body class="home">
 <!--[if lt IE 7]>
@@ -37,136 +34,67 @@
 <![endif]--> 
 
 <header>
-    <nav>
-        <div class="tnav">
-            <ul class="tnav_list">
-                <li class="tnav_item"><a href="/" class="tnav_link tnav_link-home">Home</a></li>
-                <li class="tnav_item"><a href="/members/" class="tnav_link tnav_link-members">03:06 Members</a></li>
-                <li class="tnav_item"><a href="/staff/" class="tnav_link tnav_link-staff">03:06 Staff</a></li>
-                <li class="tnav_item"><a href="/rpg/" class="tnav_link tnav_link-rpg">03:06 RPG's</a></li>
-                <li class="tnav_item"><a href="/wiki/" class="tnav_link tnav_link-wki">03:06 Wiki</a></li>
-                <li class="tnav_item"><a href="/social/" class="tnav_link tnav_link-social">03:06 Social</a></li>
-                <li class="tnav_item"><a href="/news/" class="tnav_link tnav_link-news">03:06 News</a></li>
-                <li class="tnav_item"><a href="/stats/" class="tnav_link tnav_link-stats">03:06 Stats</a></li>
-                <li class="tnav_item"><a href="/contact/" class="tnav_link tnav_link-contact">Contact 03:06</a></li>
+    <div class="tnav">
+        <div class="wrapper">
+            <ul class="tnav_list no-style">
+                <li class="tnav__item"><a href="/" class="tnav__link tnav__link-home">Home</a></li>
+                <li class="tnav__item"><a href="/members/" class="tnav__link tnav__link-members">03:06 Members</a></li>
+                <li class="tnav__item"><a href="/staff/" class="tnav__link tnav__link-staff">03:06 Staff</a></li>
+                <li class="tnav__item"><a href="/rpg/" class="tnav__link tnav__link-rpg">03:06 RPG's</a></li>
+                <li class="tnav__item"><a href="/wiki/" class="tnav__link tnav__link-wki">03:06 Wiki</a></li>
+                <li class="tnav__item"><a href="/social/" class="tnav__link tnav__link-social">03:06 Social</a></li>
+                <li class="tnav__item"><a href="/news/" class="tnav__link tnav__link-news">03:06 News</a></li>
+                <li class="tnav__item"><a href="/stats/" class="tnav__link tnav__link-stats">03:06 Stats</a></li>
+                <li class="tnav__item"><a href="/contact/" class="tnav__link tnav__link-contact">Contact 03:06</a></li>
             </ul>
         </div>
-    </nav>
+    </div>
     <div class="headerbg">
         <div class="headimg">
             <div class="wrapper">
 
                 <div class="row">
-                    <div class="col logo_main">
-                        <a href="/">
-                            <img src="/assets/img/sixpastthree-logo.png" alt="Six Past Three">
-                        </a>
+                    <div class="col col6-12 logo_main">
+                        <div class="col-inner">
+                            <a href="/">
+                                <img src="/assets/img/sixpastthree-logo.png" alt="Six Past Three">
+                            </a>
+                        </div>
                     </div>
-                    <div class="col search_form fr">
-                        <form action="" id="sform" method="post" class="sform">
-                            <label for="s" class="label sform_label">Search Me...</label>
-                            <input type="text" id="s" name="s" value="" class="sform_input input_text" placeholder="Search Me...">
-                            <input type="submit" value="Search" class="input_submit sform_submit">
-                        </form>
+                    <div class="col col6-12 search_form fr">
+                        <div class="col-inner">
+                            <form action="" id="sform" method="post" class="sform">
+                                <label for="s" class="label sform__label js-hide">Search Me...</label>
+                                <input type="text" id="s" name="s" value="" class="sform__input input_text" placeholder="Search Me...">
+                                <input type="submit" value="Search" class="input_submit sform__submit">
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
 
         </div>
     </div>
-    <nav>
-        <div class="bnav">
-            <ul class="bnav_list">
-                <li class="bnav_item">Articles:</li>
-                <li class="bnav_item"><a href="/staff/" class="bnav_link">Games</a></li>
-                <li class="bnav_item"><a href="/rpg/" class="bnav_link">Music</a></li>
-                <li class="bnav_item"><a href="/wiki/" class="bnav_link">Film &amp; TV</a></li>
-                <li class="bnav_item"><a href="/social/" class="bnav_link">Current Events</a></li>
-                <li class="bnav_item"><a href="/news/" class="bnav_link">Art &amp; Photography</a></li>
-                <li class="bnav_item"><a href="/stats/" class="bnav_link">Lifestyle</a></li>
-                <li class="bnav_item"><a href="/contact/" class="bnav_link">Gallery</a></li>
-                <li class="bnav_item"><a href="/spm/" class=""><span class="bt_text">Join 03:06</span><span class="bt_icon"></a></li>
+    <div class="tnav tnav-bottom">
+        <div class="wrapper">
+            <ul class="tnav__list no-style rel">
+                <li class="tnav__item">Articles:</li>
+                <li class="tnav__item"><a href="/staff/" class="tnav__link">Games</a></li>
+                <li class="tnav__item"><a href="/rpg/" class="tnav__link">Music</a></li>
+                <li class="tnav__item"><a href="/wiki/" class="tnav__link">Film &amp; TV</a></li>
+                <li class="tnav__item"><a href="/social/" class="tnav__link">Current Events</a></li>
+                <li class="tnav__item"><a href="/news/" class="tnav__link">Art &amp; Photography</a></li>
+                <li class="tnav__item"><a href="/stats/" class="tnav__link">Lifestyle</a></li>
+                <li class="tnav__item"><a href="/contact/" class="tnav__link">Gallery</a></li>
+                <li class="tnav__item"><a href="/spm/" class="bt"><span class="bt__text">Join 03:06</span><i class="bt__icon"></i></a></li>
             </ul>
         </div>
-    </nav>
+    </div>
+
+
 </header>
 
-<div class="whitestrip">
-    <div class="wrapper">
-        <div class="slider">
-            <ul>
-                <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
-                <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
-                <li><a href="" target="_blank"><img src="/assets/img/slider01.jpg" alt="Banner alt text"></a></li>
-            </ul>
-        </div>
-    </div>
-</div>
 
-<div class="greystrip">
-    <div class="wrapper">
 
-        <div id="features" class="row">
-            <a href="" class="col features_link">
-                <img src="/assets/img/feature.jpg" alt="">
-                <span class="features_title">Feature Link Title</span>
-            </a>
-            <a href="" class="col features_link">
-                <img src="/assets/img/feature.jpg" alt="">
-                <span class="features_title">Feature Link Title</span>
-            </a>
-            <a href="" class="col features_link">
-                <img src="/assets/img/feature.jpg" alt="">
-                <span class="features_title">Feature Link Title</span>
-            </a>
-            <a href="" class="col features_link">
-                <img src="/assets/img/feature.jpg" alt="">
-                <span class="features_title">Feature Link Title</span>
-            </a>
-        </div>
-
-        <div class="row">
-            <div class="col block">
-                <h1 class="h2 block_title">Hello Fellow Procrastinators and Welcome to Six Past <span class="featcolor">Three</span></h1>
-                <div class="block_content">
-                    <p>For almost a decade the Six Past Three group have been a collaboration of unique minds seeking refuge from their busy lives. People who are creative and imaginative seeking equally <a href="/forum">creative outlets</a>.</p>
-                    <p>Your visit to our humble home can consist of ....</p>
-                    <ul>
-                        <li>Participating in a diverse range of topics from the serious to just plain stupid.</li>
-                        <li>Contributing *squee(s)* to a range fan forum discussions and articles.</li>
-                        <li>Getting to know people from all around the world with different interests.</li>
-                        <li>Showcasing your creative aptitudes and receiving instant feedback.</li>
-                        <li>Learning something about yourself you may not have fully realised among new friends.</li>
-                    </ul>
-                    <p>The possibilities are endless.</p>
-                    <p>So with the off chance we do actually inspire something within you or even just because we're somewhat entertaining, then by all means- come join in on the fun. You may also eventually start calling Six Past Three home too.</p>
-
-                    <img src="/assets/img/team-signature.png" alt="teh 03:06 Team xxx" class="block_signature">
-                </div>
-            </div>
-            <div class="col block">
-                <h2 class="h2 title">Six Past <span class="featcolor">Three</span> News Categories</h2>
-                <div class="block_content-nav">
-                    <nav>
-                        <ul class="block_menu">
-                            <li class="block_li"><a href="" class="block_link">Forum Modifications</a></li>
-                            <li class="block_li"><a href="" class="block_link">New Features</a></li>
-                            <li class="block_li"><a href="" class="block_link">Art Challenge Results</a></li>
-                            <li class="block_li"><a href="" class="block_link">Category Title Extras</a></li>
-                            <li class="block_li"><a href="" class="block_link">Category Title Extras</a></li>
-                            <li class="block_li"><a href="" class="block_link">Category Title Extras</a></li>
-                            <li class="block_li"><a href="" class="block_link">Category Title Extras</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>        
-
-    </div>
-</div>
-
-<?php 
-  include_once('layout/footer-assets.php');
-?>
 </body>
 </html>
